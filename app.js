933c4c956ae0ee58e6fede2ef1029fe7cded5abc
olliepayne <olliepayne@me.com>
2021-03-20 22:02:27 -0700

    backup

--- app.js
+++ app.js
@@ -4,17 +4,30 @@
 
 function evaluate() {
   let equation = '5+5'
+  const operators = ['*', '/', '+', '-']
 
   const getExpressions = () => {
+    const expressions =  []
+
     for (let i = 0; i < equation.length; i++) {
-      if (i === '*' || i === '/' || i === '+' || i === '-') {
-        
+      let startIndex = 0
+      if (operators.includes(equation[i])) {
+        for (let j = i + 1; j < equation.length; j++) {
+          if (operators.includes(equation[j]) || j === equation.length - 1) {
+            expressions.push(equation.slice(startIndex, j === equation.length - 1 ? j + 1 : j))
+            startIndex = j + 1
+          }
+        }
       }
     }
+    return expressions
 
     function sortExpressions() {
 
     }
   }
   const expressions = getExpressions()
+  console.log(expressions)
 }
+
+evaluate()
